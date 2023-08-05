9d5db7cfb2a9bd3b4f55c638228ad13ae6662962
Fatemeh Kianie <112899984+fatemehkianie@users.noreply.github.com>
2023-08-05 10:49:25 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import GlobalState from "./containers/GlobalState";
 import App from "./containers/App";
 
 render(
     <GlobalState>
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </GlobalState>,
     document.getElementById("root")
 );
