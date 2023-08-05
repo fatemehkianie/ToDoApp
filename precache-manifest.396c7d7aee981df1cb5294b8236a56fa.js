a10ed6a80b35062ad99a146ceafcbcb42a62049b
Fatemeh Kianie <fatemeh.kianie@gmail.com>
2023-08-05 11:01:18 +0430

    Updates

---
+++ precache-manifest.396c7d7aee981df1cb5294b8236a56fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2865fc56c442c37295c65d2d88c86d05",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "77c9f91f59aa7a5a12c7",
+    "url": "/todoapp/static/js/2.bb293569.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/todoapp/static/js/2.bb293569.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bf82ceba0c8672d5a05",
+    "url": "/todoapp/static/js/main.6e903d0d.chunk.js"
+  },
+  {
+    "revision": "63ed8d88c0187d779e28",
+    "url": "/todoapp/static/js/runtime-main.47b19ebb.js"
+  }
+]);
